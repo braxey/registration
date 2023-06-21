b9a1364df2ca48bf06c44a92c012e47364c8ffef
Bradley Johnson <bradleyjohnson0009@gmail.com>
2023-06-20 20:11:53 -0400

    Improve past appt logic

---
+++ public/js/appt/createedit.js
@@ -0,0 +1,182 @@
+const MAX_CHAR_PER_STRING = 4
+
+$(function(){
+    let update_form = document.getElementById('update-form')
+    let create_form = document.getElementById('create-form')
+    let delete_form = document.getElementById('delete-form')
+    let form = (update_form == null) ? create_form : update_form
+    
+    // Add a submit event listener to the form (update or create)
+    form.addEventListener('submit', function(e) {
+        // Prevent the default form submission
+        e.preventDefault();
+
+        // Don't submit if the input is flawed
+        if(!validateInput()) return false
+
+        // Serialize the form data
+        var formData = $(form).serialize();
+
+        (update_form != null) ?
+            // Send the AJAX request
+            $.ajax({
+                url: form.action,
+                type: 'POST',
+                data: formData,
+                success: function(response) {
+                    // Handle successful response
+                    confirmPop("Success", "The appointment has been successfully updated.")
+                },
+                error: function(xhr, status, error) {
+                    // Handle error response
+                    errorPop("Error", "Form submission failed");
+                }
+            }):
+            form.submit()
+    })
+
+    // Add listener on delete
+    delete_form.addEventListener("submit", function(e){
+        e.preventDefault()
+        Swal.fire({
+            title: 'Are you sure?',
+            text: 'You will not be able to recover this appointment!',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if(result.isConfirmed){
+                delete_form.submit()
+            }
+        })
+    })
+})
+
+function validateInput(){
+    //title
+    let title = $('#title').val().toString()
+    let res = validateText(title)
+    if(res[0]==false) {
+        callErrorPop("title", res[1])
+        return false
+    }
+
+    //description
+    let description = $('#description').val().toString()
+    res = validateText(description)
+    if(res[0]==false) {
+        callErrorPop("description", res[1])
+        return false
+    }
+
+    // times
+    let start_time = new Date($('#start_time').val())
+    let end_time = new Date($('#end_time').val())
+    if(start_time >= end_time){
+        errorPop("Error", "The start time must be before the end time.")
+        return false
+    }
+
+    // number of slots
+    let slots = $('#total_slots').val().toString()
+    if(!validNumberOfSlots(slots)) return false
+
+
+    return true
+}
+
+// -1 - title empty
+// 0 - too long
+// 1 - quotation mark included
+// 2 - includes <script>
+// 3 - changed by decoding
+// 4 - clean
+function validateText(str){
+    if(str.length == 0) return [false, -1]
+    if(str.length > 50) return [false, 0]
+    if(str.includes("\"")) return [false, 1]
+
+    let strtmp = ""
+    let res = [true, 4]
+
+    // URL-decode
+    try{
+        strtmp = decodeURI(str)
+        if(strtmp != str) {
+            str = strtmp
+            res = [false, 3]
+        }
+    }catch(URIError){
+        // there could still be part of the string that can be decoded
+        res = [false, 3]
+    }
+
+    // remove <script>
+    strtmp = str.replace(new RegExp("<script>", "g"), "")
+    if(strtmp != str) {
+        str = strtmp
+        res = [false, 2]
+    }
+    return res
+}
+
+function validNumberOfSlots(slots){
+    // make sure the input is an integer and the number isn't unreasonably big
+    if (!isInteger(slots) || slots.length >= MAX_CHAR_PER_STRING){
+        errorPop('Error', 'Must enter a valid number.')
+        return false
+    }
+
+    // make sure the number is above 0
+    slots = parseInt(slots)
+    if (slots <= 0){
+        errorPop('Error', 'The number must be above 0.')
+        return false
+    }
+    return true
+}
+
+function isInteger(str) {
+    if (typeof str != "string") return false // we only process strings!  
+    return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+           !isNaN(parseFloat(str)) && // ...and ensure strings of whitespace fail
+           !str.includes('.') // don't allow floats
+}
+
+function callErrorPop(form_field, error_code){
+    switch(error_code){
+        case -1:
+            errorPop("Error", "The "+form_field+" cannot be empty.")
+            break
+        case 0:
+            errorPop("Error", "The "+form_field+" cannot be more than 50 characters.")
+            break
+        case 1:
+            errorPop("Error", "The "+form_field+" cannot include quotation marks.")
+            break
+        case 3:
+            errorPop("Error", "The "+form_field+" contains invalid characters.")
+            break
+        case 2:
+            errorPop("Error", "The "+form_field+" is invalid.")
+            break
+    }
+}
+
+function confirmPop(_title, _text){
+    Swal.fire({
+        title: _title,
+        text: _text,
+        icon: "success",
+        confirmButtonText: 'OK'
+    })
+}
+
+function errorPop(_title, _text){
+    Swal.fire({
+        title: _title,
+        text: _text,
+        icon: 'error',
+        confirmButtonText: 'OK'
+    })
+}
---
+++ resources/views/appointments/create.blade.php
@@ -0,0 +1,44 @@
+<x-app-layout>
+    <html>
+    <head>
+        <script src="{{asset('js/dist/jquery.min.js')}}"></script>
+        <script src="{{asset('js/dist/sweetalert2.all.min.js')}}"></script>
+    </head>
+    <body>
+    <div class="container">
+        <h1>Edit Appointment</h1>
+
+        <form action="{{ route('appointment.create') }}" method="POST" id="create-form">
+            @csrf
+            <div class="form-group">
+                <label for="title">Title</label>
+                <input type="text" name="title" id="title" class="form-control">
+            </div>
+
+            <div class="form-group">
+                <label for="description">Description</label>
+                <textarea name="description" id="description" class="form-control">Description</textarea>
+            </div>
+
+            <div class="form-group">
+                <label for="start_time">Start Time</label>
+                <input type="datetime-local" name="start_time" id="start_time" class="form-control" value="{{ now() }}">
+            </div>
+
+            <div class="form-group">
+                <label for="end_time">End Time</label>
+                <input type="datetime-local" name="end_time" id="end_time" class="form-control" value="{{ now()->addHours(1) }}">
+            </div>
+
+            <div class="form-group">
+                <label for="total_slots">Total Slots</label>
+                <input type="text" name="total_slots" id="total_slots" class="form-control" value="{{ 1 }}">
+            </div>
+
+            <button type="submit" class="btn btn-primary">Create</button>
+        </form>
+        <script type="module" src="{{ asset('js/appt/createedit.js') }}"></script>
+    </div>
+    </body>
+    </html>
+</x-app-layout>
