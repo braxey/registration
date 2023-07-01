f1afc6117d32fbb8a1448ac649060b53a0a12f06
Gareth Johnson <garethjohnson888@gmail.com>
2023-07-01 19:08:05 -0400

    Center info on pages

--- resources/views/appointments/index.blade.php
+++ resources/views/appointments/index.blade.php
@@ -2,42 +2,44 @@
     use App\Models\AppointmentUser;
 @endphp
 <x-app-layout>
-    <div>
-        <h1>Appointments</h1>
+    <div class="flex justify-center items-center h-screen">
+        <div>
+            <h1>Appointments</h1>
 
-        <table class="border border-slate-300">
-            <thead>
-                <tr class="border border-slate-300">
-                    <th class="border border-slate-300">Title</th>
-                    <th class="border border-slate-300">Start Time</th>
-                    <th class="border border-slate-300">Slots Open</th>
-                    <th class="border border-slate-300">Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($appointments as $appointment)
+            <table class="border border-slate-300">
+                <thead>
                     <tr class="border border-slate-300">
-                        <td class="border border-slate-300">{{ $appointment->title }}</td>
-                        <td class="border border-slate-300">{{ $appointment->start_time }}</td>
-                        <td class="border border-slate-300">{{ max($appointment->total_slots - $appointment->slots_taken, 0) }}</td>
-                        <td class="border border-slate-300">
-                            @if ($user && $user->admin)
-                                <a href="{{ route('appointment.edit', $appointment->id) }}">Edit</a>
-                            @endif
-                            @if ($appointment->start_time < now())
-                                Closed  
-                            @elseif ($user?->id && AppointmentUser::where('user_id', $user->id)->where('appointment_id', $appointment->id)->exists())
-                                <a href="{{ route('appointment.editbooking', $appointment->id) }}">Edit Booking</a>
-                            @else
-                                <a href="{{ route('appointment.book', $appointment->id) }}">Book</a>
-                            @endif
-                        </td>
+                        <th class="border border-slate-300">Title</th>
+                        <th class="border border-slate-300">Start Time</th>
+                        <th class="border border-slate-300">Slots Open</th>
+                        <th class="border border-slate-300">Actions</th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
-        @if ($user && $user->admin)
-            <a href="{{ route('appointment.create_form') }}">Create</a>
-        @endif
+                </thead>
+                <tbody>
+                    @foreach ($appointments as $appointment)
+                        <tr class="border border-slate-300">
+                            <td class="border border-slate-300">{{ $appointment->title }}</td>
+                            <td class="border border-slate-300">{{ $appointment->start_time }}</td>
+                            <td class="border border-slate-300">{{ max($appointment->total_slots - $appointment->slots_taken, 0) }}</td>
+                            <td class="border border-slate-300">
+                                @if ($user && $user->admin)
+                                    <a href="{{ route('appointment.edit', $appointment->id) }}">Edit</a>
+                                @endif
+                                @if ($appointment->start_time < now())
+                                    Closed  
+                                @elseif ($user?->id && AppointmentUser::where('user_id', $user->id)->where('appointment_id', $appointment->id)->exists())
+                                    <a href="{{ route('appointment.editbooking', $appointment->id) }}">Edit Booking</a>
+                                @else
+                                    <a href="{{ route('appointment.book', $appointment->id) }}">Book</a>
+                                @endif
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            @if ($user && $user->admin)
+                <a href="{{ route('appointment.create_form') }}">Create</a>
+            @endif
+        </div>
     </div>
 </x-app-layout>
