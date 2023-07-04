bd71fc278dd1f5516bde731f56f972ae340ca423
Bradley Johnson <bradleyjohnson0009@gmail.com>
2023-07-04 14:52:03 -0400

    Customize page titles

--- resources/views/appointments/confirmation.blade.php
+++ resources/views/appointments/confirmation.blade.php
@@ -1,7 +1,14 @@
 <x-app-layout>
-    <div class="flex justify-center items-center h-screen text-center">    
-        <div>
-            Confirmation
-        </div>
-    </div>
+    <html>
+        <head>
+            <title>Confirmation</title>
+        </head>
+        <body>
+            <div class="flex justify-center items-center h-screen text-center">    
+                <div>
+                    Confirmation
+                </div>
+            </div>
+        </body>
+    </html>
 </x-app-layout>
