69914a56de8788dcca5aa7a60d8cf2024ab6bf83
Bradley Johnson <bradleyjohnson0009@gmail.com>
2023-07-09 13:37:46 -0400

    SMS notis p2

---
+++ resources/views/appointments/confirmation.blade.php
@@ -0,0 +1,14 @@
+<x-app-layout>
+    <html>
+        <head>
+            <title>Confirmation</title>
+        </head>
+        <body>
+            <div class="flex justify-center items-center h-screen text-center">    
+                <div>
+                    Confirmation
+                </div>
+            </div>
+        </body>
+    </html>
+</x-app-layout>
