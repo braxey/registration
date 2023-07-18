d3e528faed6df881044b6f94887653860271dfc9
Bradley Johnson <bradleyjohnson0009@gmail.com>
2023-07-17 23:26:55 -0400

    Add tests for deleting appointments

--- tests/Feature/Appointments/AppointmentsTest.php
+++ tests/Feature/Appointments/AppointmentsTest.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
 use App\Models\User;
 use App\Models\Appointment;
+use App\Models\AppointmentUser;
 
 class AppointmentsTest extends TestCase {
 
@@ -584,6 +585,130 @@ class AppointmentsTest extends TestCase {
         $response->assertSessionHasErrors('total_slots');
     }
 
+    /* ========== DELETE APPOINTMENT ========== */
+
+    public function testNonAdminsCannotDeleteAppointments(){
+        // Create a non-admin user for testing
+        $user = User::factory()->create();
+
+        // Authenticate as the non-admin user
+        $this->actingAs($user);
+
+        // Create an appointment
+        $appointment = Appointment::factory()->create();
+
+        // Send a POST request to delete the appointment
+        $response = $this->post(route('appointment.delete', $appointment->id));
+
+        // Assert that the response status is a redirect
+        $response->assertStatus(404);
+
+        // Assert that the appointment was not deleted
+        $this->assertDatabaseHas('appointments', ['id' => $appointment->id]);
+    }
+
+    public function testAdminCanDeleteAppointmentAndReallocateSlots(){
+        // Create an admin user for testing
+        $admin = User::factory()->admin()->create();
+
+        // Authenticate as the admin user
+        $this->actingAs($admin);
+
+        // Create an appointment
+        $appointment = Appointment::factory()->create(['past_end' => false]);
+
+        // Create two users
+        $user1 = User::factory()->create();
+        $user2 = User::factory()->create();
+
+        // Book slots for the appointment
+        AppointmentUser::create([
+            'appointment_id' => $appointment->id,
+            'user_id' => $user1->id,
+            'slots_taken' => 2,
+        ]);
+        $user1->slots_booked = 2;
+        $user1->save();
+
+        AppointmentUser::create([
+            'appointment_id' => $appointment->id,
+            'user_id' => $user2->id,
+            'slots_taken' => 3,
+        ]);
+        $user2->slots_booked = 3;
+        $user2->save();
+
+        // Send a POST request to delete the appointment
+        $response = $this->post(route('appointment.delete', $appointment->id));
+
+        // Assert that the response status is a redirect
+        $response->assertStatus(302);
+
+        // Assert that the appointment was deleted
+        $this->assertDatabaseMissing('appointments', [
+            'id' => $appointment->id,
+        ]);
+
+        // Assert that the user-appointment entries were deleted
+        $this->assertDatabaseMissing('appointment_user', [
+            'appointment_id' => $appointment->id,
+            'user_id' => $user1->id,
+        ]);
+        $this->assertDatabaseMissing('appointment_user', [
+            'appointment_id' => $appointment->id,
+            'user_id' => $user2->id,
+        ]);
+
+        // Assert that the users' slots were updated
+        $this->assertEquals(0, $user1->fresh()->slots_booked);
+        $this->assertEquals(0, $user2->fresh()->slots_booked);
+    }
+
+    public function testAdminCanDeleteAppointmentWithoutReallocatingSlotsIfPastEnd(){
+        // Create an admin user for testing
+        $admin = User::factory()->admin()->create();
+
+        // Authenticate as the admin user
+        $this->actingAs($admin);
+
+        // Create a past_end appointment
+        $appointment = Appointment::factory()->create(['past_end' => true]);
+
+        // Create a user
+        $user = User::factory()->create();
+
+        // Book slots for the appointment
+        AppointmentUser::create([
+            'appointment_id' => $appointment->id,
+            'user_id' => $user->id,
+            'slots_taken' => 2,
+        ]);
+        $user->slots_booked = 2;
+        $user->save();
+
+
+        // Send a POST request to delete the appointment
+        $response = $this->post(route('appointment.delete', $appointment->id));
+
+        // Assert that the response status is a redirect
+        $response->assertStatus(302);
+
+        // Assert that the appointment was deleted
+        $this->assertDatabaseMissing('appointments', [
+            'id' => $appointment->id,
+        ]);
+
+        // Assert that the user-appointment entry was deleted
+        $this->assertDatabaseMissing('appointment_user', [
+            'appointment_id' => $appointment->id,
+            'user_id' => $user->id,
+        ]);
+
+        // Assert that the user's slots were not updated
+        $this->assertEquals(2, $user->fresh()->slots_booked);
+    }
+
+
     private function addAppointments(){
         for($i = 0 ; $i < 3 ; $i++){
             $upcomingAppointments = Appointment::factory()->create([
@@ -610,5 +735,4 @@ class AppointmentsTest extends TestCase {
             ]);
         }
     }
-
 }
