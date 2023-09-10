973776f865fc81898d9ef68039203b8ff58855fb
Bradley Johnson <bradleyjohnson0009@gmail.com>
2023-09-10 01:25:48 -0400

    Add name to notification email

--- resources/views/emails/verify-email.blade.php
+++ resources/views/emails/verify-email.blade.php
@@ -1,15 +1,14 @@
 <x-guest-layout>
     <x-authentication-card>
-        <x-slot name="logo">
-            <img src="{{ asset('images/kabc-logo.png') }}" alt="Custom Logo">
-        </x-slot>
 
         <div class="mb-4 text-sm text-gray-600">
             {{ __('Your Walk Thru Bethlehem verification code is:') }}
         </div>
 
-        <div class="mt-4 flex items-center">
-            {{ $code }}
+        <br>
+
+        <div class="mt-4 flex justify-center items-center h-screen">
+            <b>{{ $code }}</b>
         </div>
     </x-authentication-card>
 </x-guest-layout>
