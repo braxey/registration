bb5760f08aae86d16490d67551672245a4657921
Bradley Johnson <bradleyjohnson0009@gmail.com>
2023-11-25 18:47:05 -0500

    remove unused import

--- app/Http/Controllers/PasswordResetController.php
+++ app/Http/Controllers/PasswordResetController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Mail;
 use App\Services\MailerService;
 use App\Models\User;
 use App\Models\PhoneVerification;
