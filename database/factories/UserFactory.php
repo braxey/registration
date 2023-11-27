0ac3ddddf3704c353f82dd8f806c72ea257bf340
Bradley Johnson <bradleyjohnson0009@gmail.com>
2023-11-27 01:22:21 -0500

    make walk-in factory and walk-in middleware test

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -7,22 +7,11 @@ use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 use Laravel\Jetstream\Features;
-use Faker\Provider\PhoneNumber;
 
 class UserFactory extends Factory
 {
-    /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
     protected $model = User::class;
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
