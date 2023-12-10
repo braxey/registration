833a89e5e5ec29f9a92e43f57cb3c22ac7680834
Bradley Johnson <bradleyjohnson0009@gmail.com>
2023-12-10 17:11:29 -0500

    add model for queued email

---
+++ app/Models/QueuedEmail.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class QueuedEmail extends Model
+{
+    use HasFactory;
+
+    /**
+     * @var string
+     */
+    protected $table = 'email_queue';
+
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'to_address', 'email_type', 'payload', 'sent',
+    ];
+
+    /**
+     * @var array
+     */
+    protected $casts = [
+        'sent' => 'boolean',
+    ];
+
+    /**
+     * Check if the email has been queued for over an hour.
+     *
+     * @return bool
+     */
+    public function isQueuedForOverAnHour(): bool
+    {
+        return $this->created_at->isBefore(Carbon::now()->subHour());
+    }
+
+    /**
+     * Check if the email was sent.
+     *
+     * @return bool
+     */
+    public function wasSent(): bool
+    {
+        return (int) $this->sent === 1;
+    }
+
+    /**
+     * Get the to email address.
+     *
+     * @return string
+     */
+    public function getTo(): string
+    {
+        return $this->to_address;
+    }
+
+    /**
+     * Get the payload for the email.
+     *
+     * @return array
+     */
+    public function getPayload(): array
+    {
+        return json_decode($this->payload, true);
+    }
+
+    /**
+     * Get the email type.
+     *
+     * @return int
+     */
+    public function getEmailType(): int
+    {
+        return $this->email_type;
+    }
+}
