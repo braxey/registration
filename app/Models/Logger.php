69d743e62e68e02cd7dc0d985055d6b73885d7ce
Bradley Johnson <bradleyjohnson0009@gmail.com>
2024-03-15 14:11:09 -0400

    add unit tests for the new logger

--- app/Models/Logger.php
+++ app/Models/Logger.php
@@ -8,20 +8,20 @@ class Logger
 {
     public static function info(string $identifier, string $message)
     {
-        Log::info($this->createFullMessage($identifier, $message));
+        Log::info(self::createFullMessage($identifier, $message));
     }
 
     public static function error(string $identifier, string $message)
     {
-        Log::error($this->createFullMessage($identifier, $message));
+        Log::error(self::createFullMessage($identifier, $message));
     }
 
     public static function warning(string $identifier, string $message)
     {
-        Log::warning($this->createFullMessage($identifier, $message));
+        Log::warning(self::createFullMessage($identifier, $message));
     }
 
-    private function createFullMessage(string $identifier, string $message): string
+    private static function createFullMessage(string $identifier, string $message): string
     {
         return "$identifier -- $message";
     }
